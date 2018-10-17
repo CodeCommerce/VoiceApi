17c2df37572af0d5a69cd80394fb5682323be52d
C. Bauer <info@codecommerce.de>
2018-10-17 15:55:42 +0200

    Update input now get json from alexa

--- source/index.php
+++ source/index.php
@@ -1,7 +1,5 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$test_request = file_get_contents(__DIR__ . '/../AlexaRequests/StandardRequest.json');
-
-$json = json_decode($test_request);
+$json = json_decode(file_get_contents('php://input'));
 $request = new CodeCommerce\AlexaApi\Controller\RequestHandler($json);
