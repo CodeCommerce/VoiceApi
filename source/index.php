ab22a10a5eba32afd17f0ddb1416a496f19d425b
C. Bauer <info@codecommerce.de>
2018-10-18 12:18:36 +0200

    Added output for din format check

--- source/index.php
+++ source/index.php
@@ -1,8 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 // LIVE
-//$json = json_decode(file_get_contents('php://input'));
+$json = json_decode(file_get_contents('php://input'));
 
 // TEST
-$json = json_decode(file_get_contents(__DIR__ . '/../AlexaRequests/StandardRequestSlots.json'));
+//$json = json_decode(file_get_contents(__DIR__ . '/../AlexaRequests/StandardRequestSlots.json'));
 $request = new CodeCommerce\AlexaApi\Controller\RequestHandler($json);
