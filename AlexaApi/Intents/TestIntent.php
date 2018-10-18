23777cc2b324a7a3cc1642df0f132a0e08bc7818
C.Bauer <chris@codecommerce.de>
2018-10-18 15:53:48 +0200

    multiple directives

--- AlexaApi/Intents/TestIntent.php
+++ AlexaApi/Intents/TestIntent.php
@@ -65,7 +65,7 @@ class TestIntent implements IntentsInterface
         $template = new Template();
         $template->setType($template::BODY_TEMPLATE_1_SIMPLE_TEXT_IMAGES)
             ->setBackButton($template::BACKBUTTON_VISIBLE)
-            ->setBackgroundImage('https://www.codecommerce.de/wp-content/uploads/2018/04/18403105_1372829509477095_2872277146168686090_n-220x220.jpg')
+//            ->setBackgroundImage('https://www.codecommerce.de/wp-content/uploads/2018/04/18403105_1372829509477095_2872277146168686090_n-220x220.jpg')
             ->setTitle('Testtitel');
 
 
