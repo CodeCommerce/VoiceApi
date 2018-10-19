66188477637ae1aa366a00bd27830eee464b2954
C. Bauer <info@codecommerce.de>
2018-10-19 10:57:46 +0200

    Added request logging

--- source/index.php
+++ source/index.php
@@ -9,9 +9,9 @@ if ($bIsDev) {
     $json = json_decode(file_get_contents(__DIR__ . '/../AlexaRequests/StandardRequest.json'));
 } else {
     $json = json_decode(file_get_contents('php://input'));
-//
-//    $logger = new Monolog\Logger('requestlog');
-//    $f = fopen("./log/request_" . date("His_dmY") . ".log", "a+");
+
+    $logger = new Monolog\Logger('requestlog');
+    $f = fopen("../log/request_" . date("His_dmY") . ".log", "a+");
 //    $logger->pushHandler(new StreamHandler($f, $logger::WARNING));
 //    $logger->warning(file_get_contents('php://input'));
 }
