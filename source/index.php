75855dd8b578ba203c655fd82a8b99217eba9932
C. Bauer <info@codecommerce.de>
2018-10-19 10:56:42 +0200

    Added request logging

--- source/index.php
+++ source/index.php
@@ -9,11 +9,11 @@ if ($bIsDev) {
     $json = json_decode(file_get_contents(__DIR__ . '/../AlexaRequests/StandardRequest.json'));
 } else {
     $json = json_decode(file_get_contents('php://input'));
-
-    $logger = new Monolog\Logger('requestlog');
-    $f = fopen("./log/request_" . date("His_dmY") . ".log", "a+");
-    $logger->pushHandler(new StreamHandler($f, $logger::WARNING));
-    $logger->warning(file_get_contents('php://input'));
+//
+//    $logger = new Monolog\Logger('requestlog');
+//    $f = fopen("./log/request_" . date("His_dmY") . ".log", "a+");
+//    $logger->pushHandler(new StreamHandler($f, $logger::WARNING));
+//    $logger->warning(file_get_contents('php://input'));
 }
 
 $request = new CodeCommerce\AlexaApi\Controller\RequestHandler($json);
